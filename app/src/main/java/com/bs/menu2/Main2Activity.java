c7e03139631ab0691827f65ebc53adafb0b786e5
sentinelhaha <1510101226@qq.com>
2019-02-23 11:49:41 +0800

    Initial commit

---
+++ app/src/main/java/com/bs/menu2/Main2Activity.java
@@ -0,0 +1,38 @@
+package com.bs.menu2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Button btn = (Button) findViewById(R.id.registerBtn);
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(Main2Activity.this,"注册",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+}
